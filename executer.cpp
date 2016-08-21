c3dd494a2b628fb80dcc5925e5de3717b17e4e71
tsthght <tsthght@163.com>
2016-08-21 10:12:41 +0800

    add files

---
+++ executer.cpp
@@ -0,0 +1,19 @@
+//
+//  executer.cpp
+//  AtlasTestSuite
+//
+//  Created by  atlas on 16/8/18.
+//  Copyright © 2016年 GHT. All rights reserved.
+//
+
+#include "executer.hpp"
+#include "/usr/include/glib-2.0/glib.h"
+//#include <stdio.h>
+#include "Markup.h"
+
+int main() {
+    GPtrArray *p;
+    CMarkup xml;
+    printf("hello world!\n");
+    return 0;
+}
---
+++ executer.hpp
@@ -0,0 +1,14 @@
+//
+//  executer.hpp
+//  AtlasTestSuite
+//
+//  Created by  atlas on 16/8/18.
+//  Copyright © 2016年 GHT. All rights reserved.
+//
+
+#ifndef executer_hpp
+#define executer_hpp
+
+#include <stdio.h>
+
+#endif /* executer_hpp */
